424eaa442f6b5192541a2220cd52149cc827fbc5
Ankitkr <ankitkr1001a@gmail.com>
2025-09-19 22:04:16 +0530

    solved minor bugs

--- backend/index.js
+++ backend/index.js
@@ -5,31 +5,23 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 dotenv.config();
-connectDB();
 
 const app = express();
+
+
+connectDB();
+
 app.use(express.json());
 app.use(cookieParser());
 
-
-const allowedOrigins = [
-  "https://yardstick-assigment-notes-tw49.vercel.app"
-];
-
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
-
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
 
 app.options("*", cors());
 
@@ -43,7 +35,6 @@ app.use("/notes", noteRouter);
 app.use("/", planRouter);
 
 app.get("/", (req, res) => {
-  console.log("From root page");
   res.status(200).json({ success: true, message: "Welcome to the root page" });
 });
 
@@ -51,7 +42,4 @@ app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}!`);
-});
+module.exports = app;
