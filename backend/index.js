aaa294ddc322a46b8af664780bbacb15d8138c20
Ankitkr <ankitkr1001a@gmail.com>
2025-09-19 20:06:48 +0530

    added frontend link in backend

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? 'your_frontend_url' 
+    ? 'https://yardstick-assigment-notes-tw49.vercel.app/' 
     : 'http://localhost:5173',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE']
